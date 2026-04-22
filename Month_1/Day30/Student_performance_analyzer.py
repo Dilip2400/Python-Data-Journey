dd8eb8db6d2e6d584f0e151bd3c600faeecb7a55
Dilip Mandapaka <86424746+Dilip2400@users.noreply.github.com>
2026-04-22 13:53:48 +0530

    Day 30 Mini Project: Student Performance Analysis using NumPy and Pandas

---
+++ Month_1/Day30/Student_performance_analyzer.py
@@ -0,0 +1,49 @@
+#NumPy & Pandas Mini Project
+import numpy as np
+import pandas as pd  
+
+np.random.seed(0)
+
+names = ["Dilip", "Manish", "Sai", "Priya", "Sarath", "Vikas", "Uday", "Damu"]
+marks = np.random.randint(25, 100, len(names))
+
+df = pd.DataFrame({
+    "Name": names,
+    "Marks": marks
+})
+
+print(df)
+
+#Feature Engineering using NumPy
+df["Status"] = np.where(df["Marks"]>=35, "Pass", "Fail")
+
+#Grade System (Pandas)
+def grade(m):
+    if m >=90:
+        return "A+"
+    elif m >= 80:
+        return "A"
+    elif m>= 70:
+        return "B+"
+    elif m>=60:
+        return "B"
+    elif m >=35:
+        return "C"
+    else:
+        return "Fail"
+    
+df["Grades"] = df["Marks"].apply(grade)
+
+print(df.head())
+
+#Analysis using Pandas
+print("\n Average Marks: ", df["Marks"].mean())
+
+#Topper
+print("\n Topper: \n", df.loc[df["Marks"].idxmax()])
+
+#Passed Students
+print("\n Passed Students: \n", df[df["Status"]=="Pass"])
+
+#Grade Distribution
+print("\n Grade Count: \n", df["Grades"].value_counts())
