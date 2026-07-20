4118d758cf5ba5ffc1b9338d2146d207fc671ea5
Dilip Mandapaka <86424746+Dilip2400@users.noreply.github.com>
2026-07-20 14:37:08 +0530

    Gradient Descent

--- Month_3/Day65/Gradient_Descent.py
+++ Month_3/Day65/Gradient_Descent.py
@@ -3,4 +3,4 @@ errors = [800, 600, 420, 230, 100, 21, 8]
 for step, error in enumerate(errors):
     print(f"Step {step}")
     print(f"Current Error = {error}")
-    print("-"*30)
+    print("-"*30) 
